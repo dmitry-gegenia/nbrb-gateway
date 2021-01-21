28408626948fb623607cf843df55f79451043a21
Dmitry Gegenia <dmitry@gegenia.dev>
2021-01-21 12:42:32 +0300

    Добавлен повторный запрос данных если они отсутвуют на желаемую дату

--- gateway.php
+++ gateway.php
@@ -1,25 +1,39 @@
 <?php
-if (!isset($_GET['ondate'])) 
-    $on_date = date('m/d/Y');
-else  
-	if (preg_match('/^\d{2}\/\d{2}\/\d{4}$/m', $_GET['ondate']) == 0) 
-		$on_date = date('m/d/Y');
-	else 
-		$on_date = $_GET['ondate'];
 
-$dm = hash("sha256",$on_date);
-if (!file_exists('./q/'.$dm)) {
-    $curl = curl_init();        
-    curl_setopt ($curl, CURLOPT_URL, 'https://www.nbrb.by/Services/XmlExRates.aspx?ondate='.$on_date);   
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);   
+function getRatesOnNBRB($on_date,$dm) {
+    $curl = curl_init();
+    curl_setopt ($curl, CURLOPT_URL, 'https://www.nbrb.by/Services/XmlExRates.aspx?ondate='.$on_date);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);  
-    $result = curl_exec ($curl);   
-    curl_close ($curl);    
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    $result = curl_exec ($curl);
+    curl_close ($curl);
     $fp = fopen('./q/'.$dm, 'w');
     fwrite($fp,$result);
     fclose($fp);
 }
 
+if (!isset($_GET['ondate']))
+    $on_date = date('m/d/Y');
+else
+    if (preg_match('/^\d{2}\/\d{2}\/\d{4}$/m', $_GET['ondate']) == 0)
+        $on_date = date('m/d/Y');
+   else
+        $on_date = $_GET['ondate'];
+
+$dm = hash("sha256",$on_date);
+if (!file_exists('./q/'.$dm)) {
+    getRatesOnNBRB($on_date,$dm);
+}
+else {
+    if (filesize ( './q/'.$dm ) < 100 )
+       getRatesOnNBRB($on_date,$dm);
+}
+
+
 header('Content-type: text/xml');
+header('Content-Length: ' . filesize('./q/'.$dm));
+ob_clean();
+flush();
 readfile('./q/'.$dm);
+exit();
